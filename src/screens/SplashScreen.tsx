4a75d80f29287833bc589b9d30945b199e94775b
milan vala <milanvala31@gmail.com>
2023-10-04 20:00:54 +0530

    splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,28 +1,41 @@
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
 
 const SplashScreen = ({ navigation }) => {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     navigation.navigate("Login");
-  //   }, 2000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Login");
+    }, 2000);
+  }, []);
 
   return (
-    <View
-      style={{
-        backgroundColor: "black",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Text style={{ color: "white", fontSize: 36, fontWeight: "700" }}>
-        Petsmart
-      </Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/images/dogbg.png")}
+        style={styles.imageBackground}></ImageBackground>
+      <Text style={styles.title}>Petsmart</Text>
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FDD015",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageBackground: {
+    height: "100%",
+    paddingHorizontal: 20,
+    resizeMode: "repeat",
+  },
+  title: {
+    color: "black",
+    fontSize: 36,
+    fontWeight: "700",
+    position: "absolute",
+  },
+});
